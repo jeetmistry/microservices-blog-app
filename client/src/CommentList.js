d5c4b0ad953abf25262a38d8681c61898641d836
jeetmistry <jeetmistry26@gmail.com>
2022-02-06 04:50:47 +0530

    Microservices

---
+++ client/src/CommentList.js
@@ -0,0 +1,39 @@
+// import React,{useState,useEffect} from 'react';
+// import axios from 'axios';
+
+export default({comments})=>{
+    // const[comments,setComments] = useState([]);
+
+    // const fetchData = async()=>{
+    //     const res = await axios.get(`http://localhost:4001/posts/${postId}/comments`);
+
+    //     setComments(res.data);
+
+    // };
+
+    // useEffect(()=>{
+    //     fetchData();
+    // },[]);
+
+    const renderedComments = comments.map(comment=>{
+
+        let content;
+
+        if(comment.status === 'approved'){
+            content = comment.content;
+        }
+
+        if(comment.status === 'rejected'){
+            content = 'This comment is inappropriate and rejected';
+        }
+
+        if(comment.status === 'pending'){
+            content = 'This comment is awaiting moderation';
+        }
+
+        return(<li key={comment.id}>{content}</li>)
+    })
+    return(<ul>
+        {renderedComments}
+    </ul>);
+}
